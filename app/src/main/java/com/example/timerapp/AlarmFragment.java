8abd590eafbdb36e00852d2e0847aaacc6dff558
SEKI-YUTA <zard.since1991@gmail.com>
2022-06-07 21:29:30 +0900

    create layout for TimerFragment and AlarmFragment

--- app/src/main/java/com/example/timerapp/AlarmFragment.java
+++ app/src/main/java/com/example/timerapp/AlarmFragment.java
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import com.example.timerapp.Adapters.AlarmAdapter;
@@ -20,6 +22,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AlarmFragment extends Fragment implements OnAlarmToggleListener {
+    int numHour, numMin = 0;
+    NumberPicker numPickerHour, numPickerMin;
     RecyclerView recyclerView;
     FloatingActionButton fab_addAlarm;
     List<Alarm> alarmList = new ArrayList<>();
@@ -31,8 +35,13 @@ public class AlarmFragment extends Fragment implements OnAlarmToggleListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
 
+        numPickerHour = view.findViewById(R.id.numPickerHour);
+        numPickerMin = view.findViewById(R.id.numPickerMin);
         recyclerView = view.findViewById(R.id.recyclerview);
         fab_addAlarm = view.findViewById(R.id.fab_addAlarm);
+
+        setUpNumberPicker(numPickerHour);
+        setUpNumberPicker(numPickerMin);
 
         adapter = new AlarmAdapter(getContext(), alarmList, this);
 
@@ -44,16 +53,26 @@ public class AlarmFragment extends Fragment implements OnAlarmToggleListener {
             @Override
             public void onClick(View view) {
                 // add Alarm
-
-
                 // とりあえず適当なデータを追加してみる
-                Alarm newAlarm = new Alarm("07:00", false);
+                Alarm newAlarm = new Alarm(String.format("%02d:%02d", numHour, numMin), false);
                 alarmList.add(newAlarm);
                 adapter.notifyDataSetChanged();
             }
         });
 
         return view;
+    }
+
+    private void setUpNumberPicker(NumberPicker picker) {
+        if(picker.getId() == R.id.numPickerHour) {
+            picker.setMaxValue(24);
+        } else {
+            picker.setMaxValue(59);
+        }
+        picker.setMinValue(0);
+        picker.setOnValueChangedListener(listener);
+
+
     }
 
     @Override
@@ -64,4 +83,22 @@ public class AlarmFragment extends Fragment implements OnAlarmToggleListener {
             Toast.makeText(getContext(), alarm.getTime() + "のタイマーをオフにしました", Toast.LENGTH_SHORT).show();
         }
     }
+
+    NumberPicker.OnValueChangeListener listener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int prevVal, int newVal) {
+            // i prevVal
+            // i1 newVal
+            switch (numberPicker.getId()) {
+                case R.id.numPickerHour:
+                    Log.d("MyLog", "changed hour picker");
+                    numHour = newVal;
+                    break;
+                case R.id.numPickerMin:
+                    Log.d("MyLog", "changed min picker");
+                    numMin = newVal;
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/timerapp/TimerFragment.java
+++ app/src/main/java/com/example/timerapp/TimerFragment.java
@@ -7,17 +7,23 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.sql.Time;
 import java.util.Locale;
 
 public class TimerFragment extends Fragment {
-    Button btn_test;
+    int numHour, numMin, numSec;
+    NumberPicker numPickerHour, numPickerMin, numPickerSec;
+    Button btn_startAndStop, btn_cancel;
+    TextView tv_remainTime;
     MediaPlayer mediaPlayer;
     int hour, min;
     @Override
@@ -26,30 +32,50 @@ public class TimerFragment extends Fragment {
         // Inflate the layout for this fragment
         mediaPlayer = MediaPlayer.create(getContext(), R.raw.alarm1);
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
-//        btn_test = view.findViewById(R.id.btn_test);
-//
-//        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
-//            @Override
-//            public void onTimeSet(TimePicker timePicker, int i, int i1) {
-//                hour = i;
-//                min = i1;
-//                btn_test.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, min));
-//            }
-//        };
-//
-//        btn_test.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                if(mediaPlayer.isPlaying()) return;
-////                mediaPlayer.start();
-//                TimePickerDialog dialog = new TimePickerDialog(getContext(), listener, hour, min , true);
-//                dialog.show();
-//            }
-//        });
+        numPickerHour = view.findViewById(R.id.numPickerHour);
+        numPickerMin = view.findViewById(R.id.numPickerMin);
+        numPickerSec = view.findViewById(R.id.numPickerSec);
+        btn_startAndStop = view.findViewById(R.id.btn_startAndStop);
+        btn_cancel = view.findViewById(R.id.btn_cancel);
+        tv_remainTime = view.findViewById(R.id.tv_remainTime);
 
 
-
+        setUpNumberPicker(numPickerHour);
+        setUpNumberPicker(numPickerMin);
+        setUpNumberPicker(numPickerSec);
 
         return view;
     }
+
+    private void setUpNumberPicker(NumberPicker picker) {
+        if(picker.getId() == R.id.numPickerHour) {
+            picker.setMaxValue(24);
+        } else {
+            picker.setMaxValue(59);
+        }
+        picker.setMinValue(0);
+        picker.setOnValueChangedListener(listener);
+    }
+
+    NumberPicker.OnValueChangeListener listener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int prevVal, int newVal) {
+            // i prevVal
+            // i1 newVal
+            switch (numberPicker.getId()) {
+                case R.id.numPickerHour:
+                    Log.d("MyLog", "changed hour picker");
+                    numHour = newVal;
+                    break;
+                case R.id.numPickerMin:
+                    Log.d("MyLog", "changed min picker");
+                    numMin = newVal;
+                    break;
+                case R.id.numPickerSec:
+                    Log.d("MyLog", "changed min picker");
+                    numSec = newVal;
+                    break;
+            }
+        }
+    };
 }
