460d73f19d2d63845849e70d02cee3dbb72d2b79
SEKI-YUTA <zard.since1991@gmail.com>
2022-06-08 20:39:44 +0900

    add test feature

--- app/src/main/java/com/example/timerapp/TimerFragment.java
+++ app/src/main/java/com/example/timerapp/TimerFragment.java
@@ -1,12 +1,10 @@
 package com.example.timerapp;
 
-import android.app.ProgressDialog;
-import android.app.TimePickerDialog;
-import android.media.MediaPlayer;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,32 +12,58 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.sql.Time;
-import java.util.Locale;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.timerapp.Utils.MyMediaManager;
+import com.example.timerapp.Utils.MyNotificationManager;
 
 public class TimerFragment extends Fragment {
     int numHour, numMin, numSec;
     NumberPicker numPickerHour, numPickerMin, numPickerSec;
-    Button btn_startAndStop, btn_cancel;
+    Button btn_startAndStop, btn_cancel, btn_playSound, btn_stopSound, btn_pushNotification;
     TextView tv_remainTime;
-    MediaPlayer mediaPlayer;
     int hour, min;
+    MyMediaManager mediaManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mediaPlayer = MediaPlayer.create(getContext(), R.raw.alarm1);
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
         numPickerHour = view.findViewById(R.id.numPickerHour);
         numPickerMin = view.findViewById(R.id.numPickerMin);
         numPickerSec = view.findViewById(R.id.numPickerSec);
         btn_startAndStop = view.findViewById(R.id.btn_startAndStop);
         btn_cancel = view.findViewById(R.id.btn_cancel);
+        // ここからはテスト用
+        btn_playSound = view.findViewById(R.id.btn_playSound);
+        btn_stopSound = view.findViewById(R.id.btn_stopSound);
+        btn_pushNotification = view.findViewById(R.id.btn_pushNotification);
+        btn_playSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaManager = MyMediaManager.getInstance(getContext());
+                mediaManager.playSound1();
+            }
+        });
+        btn_stopSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaManager = MyMediaManager.getInstance(getContext());
+                mediaManager.stopSound1();
+            }
+        });
+        btn_pushNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyNotificationManager myNotificationManager = MyNotificationManager.getInstance(getContext());
+                myNotificationManager.pushNotification("テスト用の通知です", "テスト用の通知のコンテント");
+            }
+        });
+        // ここまでテスト用
         tv_remainTime = view.findViewById(R.id.tv_remainTime);
-
-
         setUpNumberPicker(numPickerHour);
         setUpNumberPicker(numPickerMin);
         setUpNumberPicker(numPickerSec);
@@ -48,7 +72,7 @@ public class TimerFragment extends Fragment {
     }
 
     private void setUpNumberPicker(NumberPicker picker) {
-        if(picker.getId() == R.id.numPickerHour) {
+        if (picker.getId() == R.id.numPickerHour) {
             picker.setMaxValue(24);
         } else {
             picker.setMaxValue(59);
